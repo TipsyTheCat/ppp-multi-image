b45ed1a998599cd87a1cb274b77f79f5613578cb
Pavel Zakopaylo <zakopaylo2@gmail.com>
2024-03-08 16:00:35 +1100

    Skip download for existing images

--- download.sh
+++ download.sh
@@ -2,17 +2,26 @@
 set -e
 
 BASE=$(pwd)
-rm -rf downloads
-mkdir downloads
-cd downloads
+mkdir -p downloads && cd downloads
 
-wget https://xff.cz/kernels/bootloaders-2024.04/ppp.tar.gz
-tar -xzf ppp.tar.gz
+if [ ! -d ppp ]; then
+    rm -f ppp # in case a `ppp` file exist for some reason
+    rm -f ppp.tar.gz
+
+    wget https://xff.cz/kernels/bootloaders-2024.04/ppp.tar.gz
+    tar -xzf ppp.tar.gz
+fi
+rm -f ppp.tar.gz
 
 for distro in $BASE/distros/*; do
     . $distro/config
-    wget $URL
-    $EXTRACT $(basename $URL)
+    FNAME=$(basename $URL)
+
+    if [ ! -e ${FNAME%.*} ]; then
+        rm -f $FNAME
+        wget $URL
+        $EXTRACT $FNAME
+    fi
 done
 
 cd $BASE
