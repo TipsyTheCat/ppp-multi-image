6cd50c40163ef654042b11fa04514fa7f9d60c88
Pavel Zakopaylo <zakopaylo2@gmail.com>
2024-03-08 15:29:26 +1100

    Fixed issue where boot entries would appear multiple times
    By deleting existing `boot.scr` files as per the original instructions.

--- mkimage.sh
+++ mkimage.sh
@@ -64,6 +64,7 @@ for distro in $BASE/distros/*; do
     mount /dev/disk/by-partlabel/$PARTLABEL $BASE/mounts/dst
     rsync -a $BASE/mounts/src_root/* $BASE/mounts/dst/
     rsync -a $BASE/mounts/src_boot/* $BASE/mounts/dst/boot/
+    rm -f $BASE/mounts/dst/boot/*.scr
     cp -r $distro/overrides/* $BASE/mounts/dst/
 
     umount $BASE/mounts/dst
